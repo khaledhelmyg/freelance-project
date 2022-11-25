d35ce007d57ad1b9cf92e7e1a49c0d595ee1fc65
Khalid helmy <khaledhelmyabdo12345@yahoo.com>
2022-11-25 15:57:02 +0200

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='mt-3 p-3'>
+      <h1 >Partnering for Growth</h1>
+        <p className='align-text'>
+            What we do
+            GPRS partners with owners of Australian businesses to achieve their goals, whether business expansion or a pathway to retirement.
+            <br/> 
+            <br/>
+            We are passionate about creating value for our partners and we believe strongly in aligned interests. We win when our partners win.
+        </p>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Invest.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Invest() {
+  return (
+    <div className='mt-3 p-3'>
+        <p className='align-text'>
+        <h1>The businesses we invest in</h1>
+        GPRS partners with businesses that:
+            •	Have annual pre-tax profit above $500,000 
+            •	Generate strong cash flow
+            •	Have operated profitably over multiple years
+            •	Have recurring and maintainable earnings  
+            •	Are often founder or family-owned and operated 
+            •	Have owners seeking business expansion or transition to retirement 
+        </p>
+    </div>
+  )
+}
+
+export default Invest
